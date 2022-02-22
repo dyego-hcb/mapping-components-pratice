2d00214d30cfb57c5e886ae8f7f5c72519bdd3f6
Dyego Henrique <88366871+zebodexx@users.noreply.github.com>
2022-02-22 20:54:57 -0300

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function createEntry(emojiTerm){
+  return (<Entry key={emojiTerm.id}
+    name={emojiTerm.name}
+    emoji={emojiTerm.emoji}
+    description={emojiTerm.meaning}
+  />);
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
